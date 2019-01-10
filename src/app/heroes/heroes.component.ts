b20428a9ca09ce947d1ccf715952cac2a1aa2c25
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-10 22:36:13 +0300

    Update AppRoutingModule, AppModule, and HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,7 +26,8 @@ export class HeroesComponent implements OnInit {
  
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
+  
 }
 
